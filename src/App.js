5f37ec49c7b2b31b2201cdc158f7dc8fa874dc68
JanetGM <antaly.146@gmail.com>
2019-07-01 01:14:27 -0500

    deployando con heroku

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React,{useState,useEffect} from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 import Introduction from './Components/Pages/Introduction';
 import './App.css';
