5edff6e54802359dc790676e5e4a2468ecc34c5c
JanetGM <antaly.146@gmail.com>
2019-07-01 01:13:44 -0500

    deployando con heroku

---
+++ build/precache-manifest.ee617150272e560992b7cce515e61b00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1d1ae895baad9e54dee1e94b813db1",
+    "url": "/rimac-challenge/datos-api/datos-api-move/index.html"
+  },
+  {
+    "revision": "4fc81f9454fb63c823d3",
+    "url": "/rimac-challenge/datos-api/datos-api-move/static/css/main.89ab9fa3.chunk.css"
+  },
+  {
+    "revision": "4a1a8ec83c66b84e9289",
+    "url": "/rimac-challenge/datos-api/datos-api-move/static/js/2.7e0def5b.chunk.js"
+  },
+  {
+    "revision": "4fc81f9454fb63c823d3",
+    "url": "/rimac-challenge/datos-api/datos-api-move/static/js/main.c98736f5.chunk.js"
+  },
+  {
+    "revision": "2002c96bed327ed86561",
+    "url": "/rimac-challenge/datos-api/datos-api-move/static/js/runtime~main.fd9033d0.js"
+  }
+]);
